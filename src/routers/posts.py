29ad8695b4e110e11ee3fe55ffd68cea057cbfcb
Devansh3712 <devanshamity@gmail.com>
2022-01-26 11:37:35 +0530

    Update posts route

--- src/routers/posts.py
+++ src/routers/posts.py
@@ -11,33 +11,33 @@ from ..schemas import (
     PostResponse
 )
 
-router = APIRouter()
+router = APIRouter(prefix = "/posts")
 db = Database()
 
-@router.get("/posts", response_model = List[PostResponse])
-async def get_posts():
-    posts = db.read("posts")
+@router.get("/{user}", response_model = List[PostResponse])
+async def get_posts(user: str):
+    posts = db.read(user)
     if not posts:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
-    return posts
+    return posts[1:]
 
-@router.post("/posts", response_model = PostResponse, status_code = status.HTTP_201_CREATED)
-async def create_post(post: Post):
-    posts = db.read("posts")
+@router.post("/{user}", response_model = PostResponse, status_code = status.HTTP_201_CREATED)
+async def create_post(user: str, post: Post):
+    posts = db.read(user)
     if not posts:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
     post_dict = post.dict()
     post_dict["_id"] = posts[-1]["_id"] + 1
-    result = db.insert("posts", post_dict)
+    result = db.insert(user, post_dict)
     if not result:
         raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR)
     return post_dict
 
-@router.get("/posts/{id}", response_model = PostResponse)
-async def get_post(id: int):
-    posts = db.read("posts")
-    if not posts:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
+@router.get("/{user}/{id}", response_model = PostResponse)
+async def get_post(user: str, id: int):
+    posts = db.read(user)
+    if not posts or id <= 0:
+        raise HTTPException(status_code =  status.HTTP_404_NOT_FOUND)
     result = None
     for post in posts:
         if post["_id"] == id:
@@ -49,15 +49,15 @@ async def get_post(id: int):
         )
     return result
 
-@router.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int) -> Response:
-    posts = db.read("posts")
-    if not posts:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
+@router.delete("/{user}/{id}", status_code = status.HTTP_204_NO_CONTENT)
+async def delete_post(user: str, id: int) -> Response:
+    posts = db.read(user)
+    if not posts or id <= 0:
+        raise HTTPException(status_code =  status.HTTP_404_NOT_FOUND)
     result = False
     for post in posts:
         if post["_id"] == id:
-            result = db.delete("posts", post)
+            result = db.delete(user, post)
             if not result:
                 raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR)
     if not result:
@@ -67,15 +67,15 @@ async def delete_post(id: int) -> Response:
         )
     return Response(status_code = status.HTTP_204_NO_CONTENT)
 
-@router.put("/posts/{id}", response_model = PostResponse)
-async def update_post(id: int, updated_post: Post):
-    posts = db.read("posts")
-    if not posts:
+@router.put("/{user}/{id}", response_model = PostResponse)
+async def update_post(user: str, id: int, updated_post: Post):
+    posts = db.read(user)
+    if not posts or id <= 0:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
     result = None
     for post in posts:
         if post["_id"] == id:
-            result = db.update("posts", post, updated_post.dict())
+            result = db.update(user, post, updated_post.dict())
             if not result:
                 raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR)
     if not result:
