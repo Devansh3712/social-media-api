eaa9891f3cd2562e4a06d4e82c6634bda6c5368a
Devansh3712 <devanshamity@gmail.com>
2022-01-29 22:29:23 +0530

    Implement followers/following and account privacy

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,13 +1,17 @@
+from copy import deepcopy
 from fastapi import (
     APIRouter,
+    Depends,
     HTTPException,
     status
 )
 from ..database import Database
 from ..schemas import (
+    TokenData,
     User,
     UserResponse
 )
+from ..oauth2 import get_current_user
 from ..utils import hash_password
 
 router = APIRouter(
@@ -35,3 +39,95 @@ async def get_user(username: str):
             detail = f"User {username} doesn't exist in database."
         )
     return result[0]
+
+@router.post("/follow/{username}", status_code = status.HTTP_201_CREATED)
+async def follow_user(username: str, user: TokenData = Depends(get_current_user)):
+    follower = db.read(user.username, [{ "_id": 0 }])
+    following = db.read(username, [{ "_id": 0 }])
+    if not following:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"User {username} doesn't exist in database."
+        )
+    if username == user.username:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"Cannot follow yourself."
+        )
+    if username in follower[0]["following"]:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = f"User already followed."
+        )
+    update_follower = deepcopy(follower[0])
+    update_following = deepcopy(following[0])
+    update_follower["following"].append(username)
+    update_following["followers"].append(user.username)
+    update_follower["following_count"] += 1
+    update_following["followers_count"] += 1
+    result_follower = db.update(user.username, follower[0], update_follower)
+    result_following = db.update(username, following[0], update_following)
+    if not result_follower or not result_following:
+        raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@router.post("/unfollow/{username}", status_code = status.HTTP_201_CREATED)
+async def unfollow_user(username: str, user: TokenData = Depends(get_current_user)):
+    follower = db.read(user.username, [{ "_id": 0 }])
+    following = db.read(username, [{ "_id": 0 }])
+    if not following:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"User {username} doesn't exist in database."
+        )
+    if username == user.username:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"Cannot unfollow yourself."
+        )
+    if username not in follower[0]["following"]:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = f"User not followed."
+        )
+    update_follower = deepcopy(follower[0])
+    update_following = deepcopy(following[0])
+    update_follower["following"].remove(username)
+    update_following["followers"].remove(user.username)
+    update_follower["following_count"] -= 1
+    update_following["followers_count"] -= 1
+    result_follower = db.update(user.username, follower[0], update_follower)
+    result_following = db.update(username, following[0], update_following)
+    if not result_follower or not result_following:
+        raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@router.post("/private", status_code = status.HTTP_201_CREATED)
+async def make_user_private(user: TokenData = Depends(get_current_user)):
+    result = db.read(user.username, [{ "_id": 0 }])
+    if not result:
+        raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR)
+    if result[0]["private"]:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = f"User account already private."
+        )
+    updated_result = deepcopy(result[0])
+    updated_result["private"] = True
+    update_result = db.update(user.username, result[0], updated_result)
+    if not update_result:
+        raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@router.post("/public", status_code = status.HTTP_201_CREATED)
+async def make_user_public(user: TokenData = Depends(get_current_user)):
+    result = db.read(user.username, [{ "_id": 0 }])
+    if not result:
+        raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR)
+    if not result[0]["private"]:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = f"User account already public."
+        )
+    updated_result = deepcopy(result[0])
+    updated_result["private"] = False
+    update_result = db.update(user.username, result[0], updated_result)
+    if not update_result:
+        raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR)
--- src/schemas.py
+++ src/schemas.py
@@ -1,10 +1,14 @@
-from typing import List
+from typing import (
+    List,
+    Optional
+)
 from datetime import datetime
 from pydantic import (
     BaseModel,
     conint,
     Field
 )
+from pydantic.class_validators import root_validator
 
 class Post(BaseModel):
     title: str
@@ -23,12 +27,29 @@ class User(BaseModel):
     username: str
     password: str
     posts: int = 0
+    followers: List[str] = []
+    following: List[str] = []
+    followers_count: int = 0
+    following_count: int = 0
+    private: bool = False
     timestamp: datetime = datetime.now()
 
 class UserResponse(BaseModel):
     username: str
     posts: int
+    followers: Optional[List[str]]
+    following: Optional[List[str]]
+    followers_count: int
+    following_count: int
+    private: bool
     timestamp: datetime
+    
+    @root_validator
+    def account_privacy(cls, values):
+        if values["private"]:
+            values["followers"] = None
+            values["following"] = None
+        return values
 
 class Token(BaseModel):
     access_token: str
